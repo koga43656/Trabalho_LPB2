d43169b1aa10eb1b10c928a806bb2e1ae20b6732
amaurxyz <129287512+amaurxyz@users.noreply.github.com>
2025-10-01 18:15:43 -0300

    Add files via upload

---
+++ humorlogin/humor/humor/src/main/java/com/example/humor/HumorApplication.java
@@ -0,0 +1,12 @@
+package com.example.humor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HumorApplication.class, args);
+    }
+}
