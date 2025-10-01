69a308ba10768b0a431907fd5af35d21b6f231b6
koga43656 <129287351+koga43656@users.noreply.github.com>
2025-10-01 20:47:03 -0300

    Update User.java

--- humorlogin/humor/humor/src/main/java/com/example/humor/model/User.java
+++ humorlogin/humor/humor/src/main/java/com/example/humor/model/User.java
@@ -10,16 +10,22 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String username;
+
+
+    @Column(nullable = false)
+    private String email;
+
 
     @Column(nullable = false)
     private String password;
 
     public User() {}
 
-    public User(String username, String password) {
+    public User(String username, String email, String password) {
         this.username = username;
+        this.email = email;
         this.password = password;
     }
 
@@ -27,6 +33,9 @@ public class User {
     public Long getId() { return id; }
     public String getUsername() { return username; }
     public void setUsername(String username) { this.username = username; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
     public String getPassword() { return password; }
     public void setPassword(String password) { this.password = password; }
 }
+
