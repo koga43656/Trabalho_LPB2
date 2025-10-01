8708f0d75d4dc789a5da9a996ae3b3162338ac18
koga43656 <129287351+koga43656@users.noreply.github.com>
2025-10-01 20:47:34 -0300

    Update UserRepository.java

--- humorlogin/humor/humor/src/main/java/com/example/humor/repository/UserRepository.java
+++ humorlogin/humor/humor/src/main/java/com/example/humor/repository/UserRepository.java
@@ -2,9 +2,9 @@ package com.example.humor.repository;
 
 import com.example.humor.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email); // novo
 }
